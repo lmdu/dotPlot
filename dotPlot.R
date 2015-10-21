44f30de5838a67c9d9ed80e201e79be6f1f8d91e
Lianming Du <adullb@qq.com>
2015-10-21 15:52:11 +0800

    add ggplot2 library

--- dotPlot.R
+++ dotPlot.R
@@ -1,3 +1,4 @@
+library(ggplot2)
 df <- read.table("matrix.txt", header=T, sep="\t")
 p <- ggplot(df, aes_string(x="score", y="species", size="cSSR", color="RA"))
 p <- p + geom_point() + scale_color_gradient(low="red", high="blue") + ylab("")
