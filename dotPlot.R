8864e04d1b742c5d9919e20bf2830cacab2c97c9
Lianming Du <adullb@qq.com>
2015-10-21 15:48:54 +0800

    first commit

---
+++ dotPlot.R
@@ -0,0 +1,4 @@
+df <- read.table("matrix.txt", header=T, sep="\t")
+p <- ggplot(df, aes_string(x="score", y="species", size="cSSR", color="RA"))
+p <- p + geom_point() + scale_color_gradient(low="red", high="blue") + ylab("")
+p <- p + theme_bw()
